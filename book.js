df69699b34c5e393192dbf3a21dc80b5d11abad5
Lily W <lilywu256@gmail.com>
2024-05-16 10:50:45 +0900

    Add books to page
    Created button that will allow additional books to be added to the
    library

--- book.js
+++ book.js
@@ -13,6 +13,8 @@ Book.prototype.info = function() {
 }
 
 function displayBooks() {
+    libraryContainer.innerHTML = "";
+
     for (let book of myLibrary) {
         const bookCard = document.createElement("div");
         bookCard.classList.add("card");
@@ -42,9 +44,53 @@ function displayBooks() {
         libraryContainer.appendChild(bookCard);
     }}
 
+function addBook(book) {
+    myLibrary.push(book);
+}
+
 let book1 = new Book("The Bad Beginning", "Lemony Snicket", 162, true);
 let book2 = new Book("The Reptile Room", "Lemony Snicket", 190, true);
 let book3 = new Book("The Wide Window", "Lemony Snicket", 214, false);
 
-myLibrary.push(book1, book2, book3);
+// set up homepage with 3 initial books
+addBook(book1);
+addBook(book2);
+addBook(book3);
 displayBooks();
+
+const addButton = document.querySelector("#addButton");
+const dialog = document.querySelector("#dialog");
+const submitButton = document.querySelector("#submitButton");
+const closeButton = document.querySelector("#closeButton");
+
+const titleField = document.querySelector("#title");
+const authorField = document.querySelector("#author");
+const pageField = document.querySelector("#pages");
+const readField = document.querySelector("#read");
+
+addButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+submitButton.addEventListener("click", () => {
+    let pageInput = Number(pageField.value);
+    if (pageInput <= 0 || !Number.isInteger(pageInput)) {
+        pageField.setCustomValidity("Please enter a valid page count.");
+    }
+    else {
+        pageField.setCustomValidity("");
+
+        let titleInput = titleField.value;
+        let authorInput = authorField.value;
+        if (titleInput && authorInput) {
+            let newBook = new Book(titleInput, authorInput, pageInput, readField.checked);
+            addBook(newBook);
+            displayBooks();
+        }
+    }
+});
+
+closeButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialog.close();
+});
